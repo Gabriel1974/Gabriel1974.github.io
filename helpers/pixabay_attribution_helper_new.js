24e19983814a55e9e233d5ae3fcff03c9e7d6d9e
Gabriel1974 <30882187+Gabriel1974@users.noreply.github.com>
2021-07-29 15:47:53 +1000

    Update pixabay_attribution_helper_new.js

--- helpers/pixabay_attribution_helper_new.js
+++ helpers/pixabay_attribution_helper_new.js
@@ -18,6 +18,8 @@ $(function() {
     if (url_string.indexOf("?") > 0) {
         var url = new URL(url_string);
         console.log("@GT: url = ", url);
+        var addr = url.searchParams.get("addr");
+        console.log("@GT: addr = ", addr);
         var statement = url.searchParams.get("statement");
         console.log("@GB: statement = ", statement);
 
