ed45f190478014e41a4c5456d59eae6ffb78dc83
Gabriel1974 <30882187+Gabriel1974@users.noreply.github.com>
2021-08-04 15:46:33 +1000

    Update pixabay_attribution_helper_new.js

--- helpers/pixabay_attribution_helper_new.js
+++ helpers/pixabay_attribution_helper_new.js
@@ -97,7 +97,9 @@ $(function() {
                         // Cancel the default action
                         event.preventDefault();
                     });
-                    
+            var imageURL = imageURL;           
+           var artist_id = artist_id;                                    
+           var artist = artist;
                 })
                 .fail(function() {
                     console.log("error");
@@ -111,9 +113,7 @@ $(function() {
 
             console.log("@GB: src = ", src);
             console.log("@GB: alt = ", alt);
-           var imageURL = imageURL;           
-           var artist_id = artist_id;                                    
-           var artist = artist;
+
             console.log("@GT3: imageURL(pageURL) = ", imageURL);           
             console.log("@GT3: artist_id(user_id) = ", artist_id);                                     
             console.log("@GT3: artist(user) = ", artist);
