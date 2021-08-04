9e44dbbc51776e25aae709c3d3b44da84fe7b0d0
Gabriel1974 <30882187+Gabriel1974@users.noreply.github.com>
2021-08-04 10:44:18 +1000

    Update pixabay_attribution_helper_new.js

--- helpers/pixabay_attribution_helper_new.js
+++ helpers/pixabay_attribution_helper_new.js
@@ -93,7 +93,7 @@ $(function() {
 
             var alt = url.searchParams.get("alt");
             var src = url.searchParams.get("src");
-            var imageURLGT = data.searchParams.get("pageURL");
+            var imageURLGT = url.searchParams.get("pageURL");
             var artist_idGT = data.searchParams.get("user_id");
             var artistGT = data.searchParams.get("user");
             console.log("@GB: src = ", src);
